f52b494f8fb7854e836b64244d79c8cc413080cb
Amir Mousavi <amir.mousavie@gmail.com>
2023-01-02 01:32:44 +0100

    Setup redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+
 const App = () => (
-  <div>
-    <h1>Doism</h1>
-  </div>
+  <Provider store={store}>
+    <div>
+      <h1>Doism</h1>
+    </div>
+  </Provider>
 );
 
 function render() {

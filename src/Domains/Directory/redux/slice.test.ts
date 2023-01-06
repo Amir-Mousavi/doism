e70809705590f4fd012aa25df3ea180ed8a824c7
Amir <amir.mousavie@gmail.com>
2023-01-07 00:00:26 +0100

    DB-5 Update Directory Title And Description + test

--- src/Domains/Directory/redux/slice.test.ts
+++ src/Domains/Directory/redux/slice.test.ts
@@ -34,4 +34,69 @@ describe("test reducer", () => {
       ],
     });
   });
+
+  test("should handle updateDirectoryTitleAndDescription", () => {
+    expect(
+      reducer(
+        {
+          directories: [
+            {
+              title: "New Directory",
+              description: "This is a new directory",
+              tags: ["tag1", "tag2"],
+              projects: [],
+              id: "1234",
+            },
+          ],
+        },
+        actions.updateDirectoryTitleAndDescription({
+          id: "1234",
+          title: "Updated Directory",
+          description: "This is an updated directory",
+        })
+      )
+    ).toEqual({
+      directories: [
+        {
+          title: "Updated Directory",
+          description: "This is an updated directory",
+          tags: ["tag1", "tag2"],
+          projects: [],
+          id: "1234",
+        },
+      ],
+    });
+  });
+
+  test("should handle addNewTagsToDirectory", () => {
+    expect(
+      reducer(
+        {
+          directories: [
+            {
+              title: "New Directory",
+              description: "This is a new directory",
+              tags: ["tag1", "tag2"],
+              projects: [],
+              id: "1234",
+            },
+          ],
+        },
+        actions.addNewTagsToDirectory({
+          id: "1234",
+          tags: ["tag3", "tag4"],
+        })
+      )
+    ).toEqual({
+      directories: [
+        {
+          title: "New Directory",
+          description: "This is a new directory",
+          tags: ["tag1", "tag2", "tag3", "tag4"],
+          projects: [],
+          id: "1234",
+        },
+      ],
+    });
+  });
 });
--- src/Domains/Directory/redux/slice.ts
+++ src/Domains/Directory/redux/slice.ts
@@ -16,6 +16,21 @@ export const directorySlice = createSlice({
     createDirectory: (state, action) => {
       state.directories.push(action.payload);
     },
+    updateDirectoryTitleAndDescription: (state, action) => {
+      const { id, title, description } = action.payload;
+      const directory = state.directories.find((d) => d.id === id);
+      if (directory) {
+        directory.title = title;
+        directory.description = description;
+      }
+    },
+    addNewTagsToDirectory: (state, action) => {
+      const { id, tags } = action.payload;
+      const directory = state.directories.find((d) => d.id === id);
+      if (directory) {
+        directory.tags = directory.tags.concat(tags);
+      }
+    },
   },
 });
 
